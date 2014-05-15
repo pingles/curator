34f907a8dc16a2e9640433b59c6cb1a876977150
Paul Ingles <paul@oobaloo.co.uk>
2014-05-15 21:49:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Curator
+=======
+
+Clojure library to interface with [Apache Curator](http://curator.apache.org/): "a keeper of a museum or other collection - A [ZooKeeper](http://zookeeper.apache.org) Keeper".
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject curator "0.0.1-SNAPSHOT"
+  :description "Clojurified Apache Curator"
+  :url "https://github.com/pingles/curator"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
