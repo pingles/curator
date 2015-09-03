e42f07c3bdb948c3d319f4900bf433b3f5c81426
Bernhard Streit <Bernhard.Streit@acrolinx.com>
2015-09-03 13:31:16 +0200

    Self-written (de)serialiser

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curator "1.0.0"
+(defproject curator "1.0.0-SNAPSHOT"
   :description "Clojurified Apache Curator"
   :url "https://github.com/pingles/curator"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.apache.curator/curator-recipes "2.8.0"]
                  [org.apache.curator/curator-framework "2.8.0"]
-                 [org.apache.curator/curator-x-discovery "2.8.0"]]
+                 [org.apache.curator/curator-x-discovery "2.8.0"]
+                 [org.clojure/java.data "0.1.1"]]
   :profiles {:dev {:dependencies [[org.slf4j/log4j-over-slf4j "1.7.12"]
                                   [org.slf4j/slf4j-simple "1.7.12"]]
                    :exclusions [org.slf4j/slf4j-log4j12]}}
--- src/curator/discovery.clj
+++ src/curator/discovery.clj
@@ -1,11 +1,12 @@
 (ns ^{:doc "Namespace for service discovery"} curator.discovery
-    (:require [clojure.edn :as edn]
-              [curator.framework :refer (time-units)]
-              [clojure.walk :refer (stringify-keys)])
-    (:import [org.apache.curator.x.discovery ServiceDiscovery ServiceDiscoveryBuilder ServiceInstance ServiceType UriSpec ProviderStrategy DownInstancePolicy ServiceProvider ServiceCache]
-             [org.apache.curator.x.discovery.details InstanceSerializer JsonInstanceSerializer InstanceProvider]
-             [org.apache.curator.x.discovery.strategies RandomStrategy RoundRobinStrategy StickyStrategy]
-             [java.io ByteArrayInputStream InputStreamReader PushbackReader]))
+  (:require [clojure.edn :as edn]
+    [curator.framework :refer (time-units)]
+    [clojure.data.json :as json]
+    [clojure.java.data :as data])
+  (:import [org.apache.curator.x.discovery ServiceDiscovery ServiceDiscoveryBuilder ServiceInstance ServiceType UriSpec ProviderStrategy DownInstancePolicy ServiceProvider ServiceCache]
+    [org.apache.curator.x.discovery.details InstanceSerializer JsonInstanceSerializer InstanceProvider]
+    [org.apache.curator.x.discovery.strategies RandomStrategy RoundRobinStrategy StickyStrategy]
+    [java.io ByteArrayInputStream InputStreamReader PushbackReader]))
 
 (defmacro dotonn [x & forms]
   (let [gx (gensym)]
@@ -15,7 +16,7 @@
                   `(when ~@(next f)
                      (~(first f) ~gx ~@(next f)))
                   `(~f ~gx)))
-              forms)
+           forms)
        ~gx)))
 
 (defn uri-spec*
@@ -37,111 +38,108 @@
                        :static    ServiceType/STATIC
                        :permanent ServiceType/PERMANENT}]
     (-> (dotonn (ServiceInstance/builder)
-                (.payload (if (map? payload)
-                            ;; if we got a clojure map, stringify keys in order to get nicer json output
-                            (java.util.HashMap. (stringify-keys payload))
-                            payload))
-                (.name name)
-                (.id id)
-                (.address address)
-                (.port port)
-                (.sslPort ssl-port)
-                (.uriSpec (uri-spec* uri-spec))
-                (.serviceType (service-types service-type)))
-        (.build))))
+          (.payload payload)
+          (.name name)
+          (.id id)
+          (.address address)
+          (.port port)
+          (.sslPort ssl-port)
+          (.uriSpec (uri-spec* uri-spec))
+          (.serviceType (service-types service-type)))
+      (.build))))
 
 (defn uri [^ServiceInstance service-instance]
   (.buildUriSpec service-instance))
 
-(defn json-serializer [payload-class]
-  (let [serializer (JsonInstanceSerializer. payload-class)]
-    (reify org.apache.curator.x.discovery.details.InstanceSerializer 
-      (serialize [this instance]
-        (.serialize serializer instance))
-      (deserialize [this bytes]        
-        (if (= payload-class java.util.HashMap)
-          ;; if we have a hash map, convert to clojure map
-          (let [instance (.deserialize serializer bytes)
-                {:keys [name,  id,  address,  port,  sslPort, registrationTimeUTC,  serviceType,  
-                        uriSpec]} (bean instance)]
-            (org.apache.curator.x.discovery.ServiceInstance. name id address 
-              port sslPort 
-              (clojure.walk/keywordize-keys (into {} (.getPayload instance)))
-              registrationTimeUTC,  serviceType,  uriSpec))
-          (.deserialize serializer bytes))))))
-          
-  
+(def utf-8-cs (java.nio.charset.Charset/forName "UTF-8"))
 
-  (defn ^ServiceDiscovery service-discovery
-    "base-path [/foo] 
-   payload-class [HashMap]
-   serializer [JsonInstanceSerializer (HashMap)]"
-    [curator-framework service-instance & {:keys [base-path serializer payload-class]
-                                           :or   {base-path     "/foo"
-                                                  payload-class java.util.HashMap
-                                                  serializer    (json-serializer java.util.HashMap)}}]
-    {:pre [(.startsWith ^String base-path "/")]}
-    (-> (dotonn (ServiceDiscoveryBuilder/builder payload-class)
-                (.client curator-framework)
-                (.basePath base-path)
-                (.serializer serializer)
-                (.thisInstance service-instance))
-        (.build)))
-
-  (defn services
-    "Returns the names of the services registered."
-    [^ServiceDiscovery service-discovery]
-    (.queryForNames service-discovery))
-
-  (defn random-strategy
-    []
-    (RandomStrategy. ))
-
-  (defn round-robin-strategy
-    []
-    (RoundRobinStrategy. ))
-
-  (defn sticky-strategy
-    [^ProviderStrategy strategy]
-    (StickyStrategy. strategy))
+(def json-map-serializer 
+  (reify org.apache.curator.x.discovery.details.InstanceSerializer 
+    (serialize [this instance]
+      (let  [map (data/from-java instance)
+             map-with-uri-spec (assoc map :uriSpec (.build (.getUriSpec instance)))]
+      (.getBytes (json/write-str map-with-uri-spec) utf-8-cs)))
+    (deserialize [this bytes]        
+      (let [instance (json/read-str (String. bytes utf-8-cs) )        
+            {:strs [name,  id,  address,  port,  sslPort, registrationTimeUTC,  serviceType,  
+                    uriSpec payload]} instance]
+        (org.apache.curator.x.discovery.ServiceInstance. name id address 
+         (int port ) (when sslPort (int sslPort)) payload (long registrationTimeUTC), 
+         (org.apache.curator.x.discovery.ServiceType/valueOf serviceType), 
+         (when uriSpec (org.apache.curator.x.discovery.UriSpec. uriSpec)))))))
 
 
-  (defn down-instance-policy
-    ([] (down-instance-policy 30 :seconds 2))
-    ([timeout timeout-unit error-threshold]
-       {:pre [(some time-units [timeout-unit])]}
-       (DownInstancePolicy. timeout (time-units timeout-unit) error-threshold)))
 
-  (defn ^ServiceProvider service-provider
-    "Creates a service provider for a named service s."
-    [^ServiceDiscovery service-discovery s & {:keys [strategy down-instance-policy]
-                                              :or   {strategy             (random-strategy)
-                                                     down-instance-policy (down-instance-policy)}}]
-    (-> (doto (.serviceProviderBuilder service-discovery)
-          (.serviceName s)
-          (.downInstancePolicy down-instance-policy)
-          (.providerStrategy strategy))
-        (.build)))
+(defn ^ServiceDiscovery service-discovery
+  "base-path [/foo] 
+   payload-class [clojure.lang.PersistentArrayMap]
+   serializer [JsonInstanceSerializer (clojure.lang.PersistentArrayMap)]"
+  [curator-framework service-instance & {:keys [base-path serializer payload-class]
+                                         :or   {base-path     "/foo"
+                                                payload-class clojure.lang.PersistentArrayMap
+                                                serializer    json-map-serializer}}]
+  {:pre [(.startsWith ^String base-path "/")]}
+  (-> (dotonn (ServiceDiscoveryBuilder/builder payload-class)
+        (.client curator-framework)
+        (.basePath base-path)
+        (.serializer serializer)
+        (.thisInstance service-instance))
+    (.build)))
 
-  (defn service-cache
-    "Creates a service cache (rather than reading ZooKeeper each time) for
+(defn services
+  "Returns the names of the services registered."
+  [^ServiceDiscovery service-discovery]
+  (.queryForNames service-discovery))
+
+(defn random-strategy
+  []
+  (RandomStrategy. ))
+
+(defn round-robin-strategy
+  []
+  (RoundRobinStrategy. ))
+
+(defn sticky-strategy
+  [^ProviderStrategy strategy]
+  (StickyStrategy. strategy))
+
+
+(defn down-instance-policy
+  ([] (down-instance-policy 30 :seconds 2))
+  ([timeout timeout-unit error-threshold]
+    {:pre [(some time-units [timeout-unit])]}
+    (DownInstancePolicy. timeout (time-units timeout-unit) error-threshold)))
+
+(defn ^ServiceProvider service-provider
+  "Creates a service provider for a named service s."
+  [^ServiceDiscovery service-discovery s & {:keys [strategy down-instance-policy]
+                                            :or   {strategy             (random-strategy)
+                                                   down-instance-policy (down-instance-policy)}}]
+  (-> (doto (.serviceProviderBuilder service-discovery)
+        (.serviceName s)
+        (.downInstancePolicy down-instance-policy)
+        (.providerStrategy strategy))
+    (.build)))
+
+(defn service-cache
+  "Creates a service cache (rather than reading ZooKeeper each time) for
    the service named s"
-    [^ServiceDiscovery service-discovery s]
-    (-> (.serviceCacheBuilder service-discovery)
-        ( .name s)
-        (.build)))
+  [^ServiceDiscovery service-discovery s]
+  (-> (.serviceCacheBuilder service-discovery)
+    ( .name s)
+    (.build)))
 
-  (defn note-error
-    "Clients should use this to indicate a problem when trying to
+(defn note-error
+  "Clients should use this to indicate a problem when trying to
    connect to a service instance. The instance may be marked as down
    depending on the service provider's down instance policy."
-    [^ServiceProvider service-provider ^ServiceInstance instance]
-    (.noteError service-provider instance))
+  [^ServiceProvider service-provider ^ServiceInstance instance]
+  (.noteError service-provider instance))
 
-  (defmulti instances (fn [^Object x & args] (.getClass x)))
-  (defmethod instances ServiceDiscovery [^ServiceDiscovery sd s] (.queryForInstances sd s))
-  (defmethod instances ServiceCache [^ServiceCache sc] (.getInstances sc))
+(defmulti instances (fn [^Object x & args] (.getClass x)))
+(defmethod instances ServiceDiscovery [^ServiceDiscovery sd s] (.queryForInstances sd s))
+(defmethod instances ServiceCache [^ServiceCache sc] (.getInstances sc))
 
-  (defmulti instance (fn [^Object x & args] (.getClass x)))
-  (defmethod instance ServiceProvider [^ServiceProvider provider] (.getInstance provider))
-  (defmethod instance ServiceCache [cache ^ProviderStrategy strategy] (.getInstance strategy cache))
+(defmulti instance (fn [^Object x & args] (.getClass x)))
+(defmethod instance ServiceProvider [^ServiceProvider provider] (.getInstance provider))
+(defmethod instance ServiceCache [cache ^ProviderStrategy strategy] (.getInstance strategy cache))
