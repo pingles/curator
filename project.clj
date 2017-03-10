f192b7df74066b3d84fadac4617e40834dd457e5
Paul Ingles <paul@oobaloo.co.uk>
2017-03-10 11:28:23 +0000

    release 0.0.7 with curator 3.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curator "0.0.7-SNAPSHOT"
+(defproject curator "0.0.7"
   :description "Clojurified Apache Curator"
   :url "https://github.com/pingles/curator"
   :license {:name "Eclipse Public License"
